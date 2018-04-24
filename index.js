d60951cbe880d9e1e21b445dc0e589a2d03f54fd
Kim Seungha <seungha.me@gmail.com>
2018-04-24 16:10:33 +0900

    기초 기능 완성

---
+++ index.js
@@ -0,0 +1,44 @@
+function randomNumber() {
+  return Math.floor(256 * Math.random());
+}
+
+function randomColorCode() {
+  return `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+}
+
+const colorCodeEl = document.querySelector('.color-code');
+const boxes = document.querySelectorAll('.box');
+
+let correctAnswer;
+let score = 0;
+document.querySelector('.score').textContent = score;
+
+function newStage() {
+  const colorCodes = [randomColorCode(), randomColorCode(), randomColorCode()]
+  boxes.forEach((el, index) => {
+    el.style.backgroundColor = colorCodes[index];
+  });
+  correctAnswer = Math.floor(Math.random() * 3);
+  colorCodeEl.textContent = colorCodes[correctAnswer];
+}
+
+boxes.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    if (correctAnswer === index) {
+      alert('맞았습니다!');
+      document.querySelector('.modal.right').classList.add('show');
+      score++;
+    } else {
+      alert('틀렸습니다!');
+      score = 0;
+    }
+    document.querySelector('.score').textContent = score;
+    newStage();
+  })
+})
+
+document.querySelector('.modal.right .close').addEventListener('click', () => {
+  document.querySelector('.modal.right').classList.remove('show');
+})
+
+newStage();
