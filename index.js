9c2e6776c2674e3981bb63f9e77361e2b84e0fbf
Kim Seungha <seungha.me@gmail.com>
2018-04-24 16:29:07 +0900

    large 클래스 추가

--- index.js
+++ index.js
@@ -24,20 +24,22 @@ function newStage() {
 
 boxes.forEach((el, index) => {
   el.addEventListener('click', () => {
+    el.classList.add('large');
     if (correctAnswer === index) {
-      alert('맞았습니다!');
       document.querySelector('.modal.right').classList.add('show');
       score++;
     } else {
-      alert('틀렸습니다!');
       score = 0;
     }
     document.querySelector('.score').textContent = score;
-    newStage();
   })
 })
 
 document.querySelector('.modal.right .close').addEventListener('click', () => {
+  newStage();
+  boxes.forEach(el => {
+    el.classList.remove('large');
+  });
   document.querySelector('.modal.right').classList.remove('show');
 })
 
